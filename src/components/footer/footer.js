f83fc1c32712464e763ca373dba7babc4b57adbf
SatooRu65536 <ryuto4619@gmail.com>
2023-12-27 16:00:48 +0900

    add: フッター

---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>なんか在庫管理 inc. © 2023 Copyright.</p>
+    </footer>
+  );
+}
