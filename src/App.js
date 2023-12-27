c69b30fad09941626d2a496a3c5cefe5d76eeb5d
SatooRu65536 <ryuto4619@gmail.com>
2023-12-27 17:09:05 +0900

    add: 商品管理画面

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.module.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import Manage from "./components/page/manage/manage";
 
 function App() {
   return (
     <main>
       <Header />
+      <Manage />
       <Footer />
     </main>
   );
---
+++ src/components/page/manage/manage.js
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import styles from "./manage.module.css";
+
+export default function Manage() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleNameChange(e) {
+    const value = e.target.value;
+    const upperCaseValue = value.toLowerCase();
+    setName(upperCaseValue);
+  }
+
+  function handleAdd() {
+    const newItem = {
+      name: name,
+      price: price,
+      stock: stock,
+    };
+
+    if (newItem.name === "") {
+      alert("商品名を入力してください");
+      return;
+    }
+
+    if (newItem.price === "") {
+      alert("単価を入力してください");
+      return;
+    }
+
+    if (newItem.stock === "") {
+      alert("数量を入力してください");
+      return;
+    }
+
+    if (newItem.price < 0) {
+      alert("単価は0以上を入力してください");
+      return;
+    }
+
+    if (newItem.stock < 0) {
+      alert("数量は0以上を入力してください");
+      return;
+    }
+
+    setItems([...items, newItem]);
+    setName("");
+    setPrice("");
+    setStock("");
+  }
+
+  function handleDelete(index) {
+    const newItems = items.filter((item, i) => i !== index);
+    setItems(newItems);
+  }
+
+  return (
+    <main className={styles.manage}>
+      <h1 className={styles.title}>在庫一覧</h1>
+
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>商品名</th>
+            <th>単価</th>
+            <th>数量</th>
+            <th className={styles.delete}>削除</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map((item, i) => (
+            <tr key={i}>
+              <td className={styles.name}>{item.name}</td>
+              <td>{item.price}円</td>
+              <td>{item.stock}個</td>
+              <td className={styles.delete}>
+                <input
+                  type="button"
+                  value="削除"
+                  onClick={() => handleDelete(i)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className={styles.container}>
+        <div className={styles.add_list}>
+          <label>商品名:</label>
+          <input
+            type="text"
+            placeholder="商品名"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <label>単価:</label>
+          <input
+            type="number"
+            placeholder="単価"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label>数量:</label>
+          <input
+            type="number"
+            placeholder="数量"
+            value={stock}
+            onChange={(e) => setStock(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.add}>
+          <input type="button" value="追加" onClick={handleAdd} />
+        </div>
+      </div>
+    </main>
+  );
+}
